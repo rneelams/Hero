9991159696ad733dab5d487a1ac3559d67a000df
rneelams <ramesh.neelamsetti@capgemini.com>
2020-03-11 11:52:50 +0530

    update about

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
-import { Hero } from './hero';
+import { Hero } from "./hero";
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, about: "Software engineer", name: "Ramesh" },
+  { id: 12, about: "Land Surveior", name: "Kiran" },
+  { id: 13, about: "Mechanical engineer", name: "Tarun" },
+  { id: 14, about: "Army Soldier", name: "Sai" },
+  { id: 15, about: "Student", name: "Naveen" },
+  { id: 16, about: "Software engineer", name: "Kittu" },
+  { id: 17, about: "Agricultural Specialist", name: "Jyothi" },
+  { id: 18, about: "Home Maker", name: "Swathi" },
+  { id: 19, about: "Social Teacher", name: "Pallavi" },
+  { id: 20, about: "Family Teacher", name: "Siri" }
 ];
